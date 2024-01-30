52d83805d1d9eaa2b22e51420e04a358e0d1c0b1
Oleksandra Friz <ipz221_foi@student.ztu.edu.ua>
2024-01-30 16:00:51 +0200

    додано gitignore, temporary, TechPraktZavdanna

---
+++ TechPraktZavdanna/TechPraktZavdanna/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TechPraktZavdanna
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter number: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int sum = num1 + num2;
+            Console.Write($"Suma = {sum}");
+        }
+    }
+}
